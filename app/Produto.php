b21d7ba7a796ea7c505eab5c5688043a457c8037
GuilhermeHMG <guilherme.ghg00@gmail.com>
2024-07-19 16:09:07 -0300

    IMPLEMENTANDO O CADASTRO DE PRODUTOS PARTE 1 (INDEX)

--- app/Produto.php
+++ app/Produto.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    //
+    protected $fillable = [
+        'tbpr_nome',
+        'tbpr_desc',
+        'tbpr_peso',
+        'tbpr_est_min',
+        'tbpr_est_max',
+        'tbpr_vend_var',
+        'tbpr_vend_at',
+        'tbpr_custo',
+        'id_unidade'
+    ];
 }
